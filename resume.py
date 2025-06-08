22319f3ffa1990065fb3ab3444ed4f51779db952
CorvoCS08 <gcarvalho2112@gmail.com>
2025-06-08 16:13:50 -0300

    adjusted type ignore in resume.py

--- resume.py
+++ resume.py
@@ -6,8 +6,8 @@ from youtube import manager
 load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
 
-genai.configure(api_key=api_key) # type: ignore[attr-defined]
-gemini_model = genai.GenerativeModel("models/gemini-2.0-flash") # type: ignore[attr-defined]
+genai.configure(api_key=api_key)  #type: ignore[reportPrivateImportUsage]
+gemini_model = genai.GenerativeModel("models/gemini-2.0-flash")  #type: ignore[reportPrivateImportUsage]
 
 def resume():
     with open(f"cache\\{manager.yt.video_id}\\transcription.txt", "r", encoding="utf-8") as f:
