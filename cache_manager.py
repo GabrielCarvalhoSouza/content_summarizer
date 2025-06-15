5bdd2eb06f606044ed6b65b24d7409749538b6e8
CorvoCS08 <gcarvalho2112@gmail.com>
2025-06-15 15:55:30 -0300

    Type Hint: cache_manager.py

--- cache_manager.py
+++ cache_manager.py
@@ -3,7 +3,9 @@ from pathlib import Path
 
 
 class CacheManager:
-    def create_cache(self, video_metadata: dict[str, str], metadata_file_path: Path):
+    def create_cache(
+        self, video_metadata: dict[str, str], metadata_file_path: Path
+    ) -> None:
         metadata_file_path.parent.mkdir(parents=True, exist_ok=True)
 
         with open(metadata_file_path, "w", encoding="utf-8") as f:
